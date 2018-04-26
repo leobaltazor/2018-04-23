9b03659a4f82f326d9caddedd7134fd629224c44
leobaltazor <leo.baltazor@gmail.com>
2018-04-26 18:00:38 +0300

    dz10%

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var order = [
 	{
 		"room": {
 			"number": 1,
-			"adults": 1,
+			"adults": 3,
 			"childrens": 2,
-			"childrens-age":
+			"childrensAge":
 				{
-					"children-age-1": 6,
-					"children-age-2": 6
+					"childrenAge1": 5,
+					"childrenAge2": 6
 				},
 		},
 	},
@@ -16,15 +16,16 @@ var order = [
 			"number": 2,
 			"adults": 2,
 			"childrens": 1,
-			"childrens-age":
+			"childrensAge":
 				{
-					"children-age-1": 6,
+					"childrenAge1": 6,
 				},
 		},
 	}
 ]
 
-function init(){
+function init() {
+	var orderForm = document.querySelector('.order')
 	var div = document.createElement("div");
 	var roomLabel = document.createElement("label");
 	var roomInput = document.createElement("input");
@@ -36,14 +37,90 @@ function init(){
 	roomInput.setAttribute("value", order.length);
 	div.appendChild(roomLabel);
 	div.appendChild(roomInput);
-	document.body.appendChild(div);
-for (let i = 0; i < order.length; i++) {
-	var div = document.createElement("div")
-	var label = document.createElement("label")
-	var input = document.createElement("input")
-	label.innerHTML = order[i].room.number;
-	div.appendChild(label);
-	document.body.appendChild(div);
-}
+	orderForm.innerHTML = '';
+	orderForm.appendChild(div);
+	for (let i = 0; i < order.length; i++) {
+		var divRooms = document.createElement("div")
+		var labelAdults = document.createElement("label")
+		var inputAdults = document.createElement("input")
+		var labelChildrens = document.createElement("label")
+		var inputChildrens = document.createElement("input")
+		var divchildrens = document.createElement("div")
+		//лабел для инпута взврослых каждой комнаты
+		labelAdults.innerHTML = "Количество взрослых в комнате №" + order[i].room.number;
+		labelAdults.setAttribute("for", "room-" + (i + 1) + "-adults");
+		//лабел для инпута детей каждой комнаты
+		labelChildrens.innerHTML = "Количество детей в комнате №" + order[i].room.number;
+		labelChildrens.setAttribute("for", "room-" + (i + 1) + "child");
+		//инпут взрослых каждой комнаты
+		inputAdults.setAttribute("type", "text");
+		inputAdults.setAttribute("id", "room-" + (i + 1) + "-adults");
+		inputAdults.setAttribute("size", "5");
+		inputAdults.setAttribute("value", order[i].room.adults);
+
+		//инпут детей каждой комнаты
+		inputChildrens.setAttribute("type", "text");
+		inputChildrens.setAttribute("id", "room-" + (i + 1) + "-child");
+		inputChildrens.setAttribute("size", "5");
+		inputChildrens.setAttribute("value", order[i].room.childrens);
+		for (let j = 0; j < order[i].room.childrens; j++) {
+			var labelChildrensAge = document.createElement("label")
+			var inputChildrensAge = document.createElement("input")
+			//лабел для инпута возраста каждго ребенка
+			labelChildrensAge.innerHTML = "Возраст ребенка №" + (j + 1);
+			labelChildrensAge.setAttribute("for", "room-" + (i + 1) + "-children-age-" + (j + 1));
+			//инпут возраста каждого ребенка
+			inputChildrensAge.setAttribute("type", "text");
+			inputChildrensAge.setAttribute("id", "room-" + (i + 1) + "-children-age-" + (j + 1));
+			inputChildrensAge.setAttribute("size", "5");
+			inputChildrensAge.setAttribute("value", order[i].room.childrensAge.childrenAge1); //не получается взять значение каждого ребенка
+
+			divchildrens.appendChild(labelChildrensAge);
+			divchildrens.appendChild(inputChildrensAge);
+		}
+
+		//вставляем лабел взрослых и инпут взрослых в див каждой комнаты
+		divRooms.appendChild(labelAdults);
+		divRooms.appendChild(inputAdults);
+		divRooms.appendChild(labelChildrens);
+		divRooms.appendChild(inputChildrens);
+		divRooms.appendChild(divchildrens);
+
+
+
+
+		div.appendChild(divRooms);
+	}
 }
 init()
+
+document.getElementById("room").addEventListener("change", orderRoom)
+function orderRoom() {
+	var roomCount = document.getElementById("room").value
+	if (roomCount < order.length) {
+		var minus = 0;
+		minus = order.length - roomCount;
+		for (let i = 0; i < minus; i++) {
+			order.pop()
+		}
+		init()
+	} else if (roomCount > order.length) {
+		var add = 0;
+		add = roomCount - order.length;
+		for (let i = 0; i < add; i++) {
+			var orderNew = {
+				"room": {
+					"number": order.length + 1,
+					"adults": 0,
+					"childrens": 0,
+					"childrensAge":
+						{
+							"childrenAge1": 0
+						},
+				},
+			}
+			order.push(orderNew)
+		}
+		init()
+	}
+}
